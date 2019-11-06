f8f485176394c91938ac0f5d39105ad45d049986
yarstech <49331643+yarstech@users.noreply.github.com>
2019-11-06 11:01:03 +0300

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,23 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from hh_1c import settings
+from hh_1c.spiders.hh_vacations import HhVacationsSpider
+from hh_1c.spiders.avito import AvitoSpider
+from hh_1c.spiders.avito_cars import AvitoCarsSpider
+
+from hh_1c.spiders.instagram import InstagramSpider
+from config import INSAT_LOGIN, INSAT_PASS
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+
+    process = CrawlerProcess(settings=crawler_settings)
+    #process.crawl(HhVacationsSpider)
+    #process.crawl(AvitoSpider)
+    process.crawl(AvitoCarsSpider)
+
+    #users = ['putinofficial.ru', 'timatiofficial_fanpage']
+    #process.crawl(InstagramSpider, INSAT_LOGIN, INSAT_PASS, users)
+    process.start()
